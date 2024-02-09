3ae42ce961310f9c9230ce7409cc12ae5b65ab02
Marshall Lorhan <mlorhan@gmail.com>
2024-02-08 21:18:24 -0800

    added buttons

---
+++ index.js
@@ -0,0 +1,14 @@
+let playerName = prompt("Please enter your name");
+
+if (playerName != null) {
+  document.getElementById("playerName").innerHTML =
+  return "playerName" + "/'s cards"
+}
+
+var pot = 0;
+var purse = 500;
+
+function addToPot(){
+    document.querySelector('.addFive').Numbercontent = Number;
+}
+
