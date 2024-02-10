89681afbf020b107d48394aafec24f375c900c0d
Marshall Lorhan <mlorhan@gmail.com>
2024-02-10 14:35:34 -0800

    Added create button function

--- script.js
+++ script.js
@@ -1,43 +1,90 @@
 //* add bet function to add funds to pot and and subtract funds from purse. */
 
-let betFive = document.querySelector("#betFive");
-let betTen = document.querySelector("#betTen");
-let betTwenty = document.querySelector("#betTwenty");
+/* var betOutput = 0
+var purse = 500
 
-betFive.addEventListener("onclick", function () {
-  let output = document.querySelector("#output");
+let up = betOutput
+
+
+increaseBetFive()
+increaseBetTen()
+increaseBetTwenty()
+decreasePurseFive()
+decreasePurseTen()
+decreasePurseTwenty()
+
+let betFive = document.querySelector(".betFive");
+let betTen = document.querySelector(".betTen");
+let betTwenty = document.querySelector(".betTwenty");
+
+betFive.addEventListener("click", function (e) {
+  let betOutput = document.querySelector("#betOutput");
   let result = Number(output.innerText) + 5;
 
-  if (result > 500) {
+  if (result > purse) {
     result = "Max Bet";
   }
 
-  output.innerText = result;
+  betOutput.innerText = result;
 });
 
-betTen.addEventListener("onclick", function () {
-  let output = document.querySelector("#output");
+betTen.addEventListener("click", function (e) {
+  let betOutput = document.querySelector("#betOutput");
   let result = Number(output.innerText) + 10;
 
   if (result > purse) {
     result = "Max Bet";
   }
 
-  output.innerText = result;
+  betOutput.innerText = result;
 });
 
-betTwenty.addEventListener("onclick", function () {
-  let output = document.querySelector("#output");
+betTwenty.addEventListener("click", function (e) {
+  let betOutput = document.querySelector("#betOutput");
   let result = Number(output.innerText) + 20;
 
-  if (result > 500) {
-    result = "Max Bet";
+  if (result > purse) {result = "Max Bet";
   }
 
-  output.innerText = result;
-});
+  betOutput.innerText = result;
+}); */
 
 //* add deal function to deal cards to player & dealer cards array.  Creat "Hit Me" & Stay Button */
+
+function dealCards(){}
+
+function hitMeButton(){
+	var newButton = document.createElement('button');
+    newButton.textContent = 'Hit Me!';
+    newButton.classList.add('styledButton');
+
+    newButton.addEventListener('click', function() {
+        alert('Hit Me button clicked!').class.dealCards;
+    });
+
+    var buttonContainer = document.getElementById('dealCardsContainer');
+    dealCardsContainer.appendChild(newButton);
+
+}
+
+function stayButton(){
+	var newButton = document.createElement('button');
+    newButton.textContent = 'Stay!';
+    newButton.classList.add('styledButton');
+    
+    // Add event listener to the new button (optional)
+    newButton.addEventListener('click', function() {
+        alert('Stay button clicked!');
+    });
+
+    // Append the new button to the container
+    var buttonContainer = document.getElementById('dealCardsContainer');
+    dealCardsContainer.appendChild(newButton);
+
+}
+
+
+
 
 
 //* create function to evaluate for winner & update purse */
