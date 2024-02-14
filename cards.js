2d4fd10a288edf8650d02e503060acc616f532c2
Marshall Lorhan <mlorhan@gmail.com>
2024-02-13 20:48:12 -0800

    updated cards.js file

---
+++ cards.js
@@ -0,0 +1,133 @@
+export const playerHand = [
+    { rank: 'Ace', suit: 'Hearts' },
+    { rank: 'Ace', suit: 'Diamonds' },
+    { rank: 'Ace', suit: 'Clubs' },
+    { rank: 'Ace', suit: 'Spades' },
+    
+    { rank: 'King', suit: 'Hearts' },
+    { rank: 'King', suit: 'Diamonds' },
+    { rank: 'King', suit: 'Clubs' },
+    { rank: 'King', suit: 'Spades' },
+    
+    { rank: 'Queen', suit: 'Hearts' },
+    { rank: 'Queen', suit: 'Diamonds' },
+    { rank: 'Queen', suit: 'Clubs' },
+    { rank: 'Queen', suit: 'Spades' },
+    
+    { rank: 'Jack', suit: 'Hearts' },
+    { rank: 'Jack', suit: 'Diamonds' },
+    { rank: 'Jack', suit: 'Clubs' },
+    { rank: 'Jack', suit: 'Spades' },
+    
+    { rank: '10', suit: 'Hearts' },
+    { rank: '10', suit: 'Diamonds' },
+    { rank: '10', suit: 'Clubs' },
+    { rank: '10', suit: 'Spades' },
+    
+    { rank: '9', suit: 'Hearts' },
+    { rank: '9', suit: 'Diamonds' },
+    { rank: '9', suit: 'Clubs' },
+    { rank: '9', suit: 'Spades' },
+    
+    { rank: '8', suit: 'Hearts' },
+    { rank: '8', suit: 'Diamonds' },
+    { rank: '8', suit: 'Clubs' },
+    { rank: '8', suit: 'Spades' },
+    
+    { rank: '7', suit: 'Hearts' },
+    { rank: '7', suit: 'Diamonds' },
+    { rank: '7', suit: 'Clubs' },
+    { rank: '7', suit: 'Spades' },
+    
+    { rank: '6', suit: 'Hearts' },
+    { rank: '6', suit: 'Diamonds' },
+    { rank: '6', suit: 'Clubs' },
+    { rank: '6', suit: 'Spades' },
+    
+    { rank: '5', suit: 'Hearts' },
+    { rank: '5', suit: 'Diamonds' },
+    { rank: '5', suit: 'Clubs' },
+    { rank: '5', suit: 'Spades' },
+    
+    { rank: '4', suit: 'Hearts' },
+    { rank: '4', suit: 'Diamonds' },
+    { rank: '4', suit: 'Clubs' },
+    { rank: '4', suit: 'Spades' },
+    
+    { rank: '3', suit: 'Hearts' },
+    { rank: '3', suit: 'Diamonds' },
+    { rank: '3', suit: 'Clubs' },
+    { rank: '3', suit: 'Spades' },
+    
+    { rank: '2', suit: 'Hearts' },
+    { rank: '2', suit: 'Diamonds' },
+    { rank: '2', suit: 'Clubs' },
+    { rank: '2', suit: 'Spades' },
+];
+
+export const dealerHand = [
+    { rank: 'Ace', suit: 'Hearts' },
+    { rank: 'Ace', suit: 'Diamonds' },
+    { rank: 'Ace', suit: 'Clubs' },
+    { rank: 'Ace', suit: 'Spades' },
+    
+    { rank: 'King', suit: 'Hearts' },
+    { rank: 'King', suit: 'Diamonds' },
+    { rank: 'King', suit: 'Clubs' },
+    { rank: 'King', suit: 'Spades' },
+    
+    { rank: 'Queen', suit: 'Hearts' },
+    { rank: 'Queen', suit: 'Diamonds' },
+    { rank: 'Queen', suit: 'Clubs' },
+    { rank: 'Queen', suit: 'Spades' },
+    
+    { rank: 'Jack', suit: 'Hearts' },
+    { rank: 'Jack', suit: 'Diamonds' },
+    { rank: 'Jack', suit: 'Clubs' },
+    { rank: 'Jack', suit: 'Spades' },
+    
+    { rank: '10', suit: 'Hearts' },
+    { rank: '10', suit: 'Diamonds' },
+    { rank: '10', suit: 'Clubs' },
+    { rank: '10', suit: 'Spades' },
+    
+    { rank: '9', suit: 'Hearts' },
+    { rank: '9', suit: 'Diamonds' },
+    { rank: '9', suit: 'Clubs' },
+    { rank: '9', suit: 'Spades' },
+    
+    { rank: '8', suit: 'Hearts' },
+    { rank: '8', suit: 'Diamonds' },
+    { rank: '8', suit: 'Clubs' },
+    { rank: '8', suit: 'Spades' },
+    
+    { rank: '7', suit: 'Hearts' },
+    { rank: '7', suit: 'Diamonds' },
+    { rank: '7', suit: 'Clubs' },
+    { rank: '7', suit: 'Spades' },
+    
+    { rank: '6', suit: 'Hearts' },
+    { rank: '6', suit: 'Diamonds' },
+    { rank: '6', suit: 'Clubs' },
+    { rank: '6', suit: 'Spades' },
+    
+    { rank: '5', suit: 'Hearts' },
+    { rank: '5', suit: 'Diamonds' },
+    { rank: '5', suit: 'Clubs' },
+    { rank: '5', suit: 'Spades' },
+    
+    { rank: '4', suit: 'Hearts' },
+    { rank: '4', suit: 'Diamonds' },
+    { rank: '4', suit: 'Clubs' },
+    { rank: '4', suit: 'Spades' },
+    
+    { rank: '3', suit: 'Hearts' },
+    { rank: '3', suit: 'Diamonds' },
+    { rank: '3', suit: 'Clubs' },
+    { rank: '3', suit: 'Spades' },
+    
+    { rank: '2', suit: 'Hearts' },
+    { rank: '2', suit: 'Diamonds' },
+    { rank: '2', suit: 'Clubs' },
+    { rank: '2', suit: 'Spades' },
+];
